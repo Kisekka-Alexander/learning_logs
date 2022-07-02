1f093a8a8d3c18d310bad9a2deb689f47355ec7f
Kisekka-Alexander <kkalexander717@gmail.com>
2022-07-02 03:11:43 +0300

    add topic template

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,5 +5,7 @@ app_name = 'learning_logs'
 urlpatterns = [
     #Home Page
     path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
 
 ]
