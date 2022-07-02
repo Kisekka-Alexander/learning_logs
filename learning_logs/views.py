8cd3375489f0d290cff67ae863099bcd82310956
Kisekka-Alexander <kkalexander717@gmail.com>
2022-07-02 03:37:43 +0300

    add topic def

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -12,3 +12,9 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+def topic(request, topic_id):
+    """Show a single topic and all its entries."""
+    topic = Topic.object.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
