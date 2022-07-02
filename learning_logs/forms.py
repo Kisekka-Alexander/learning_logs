f94c4e3300c2e03e4eea9f24de7c219733222580
Kisekka-Alexander <kkalexander717@gmail.com>
2022-07-02 14:00:56 +0300

    create new_topic view func

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -2,7 +2,7 @@ from dataclasses import fields
 from django import forms
 from .models import Topic
 
-class TopiForm(forms.ModelForm):
+class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields=['text']
